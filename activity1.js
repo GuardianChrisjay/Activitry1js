2839da0b5ca92040740caf6800c00789908ea485
Chrisjayrowin C. Guardian <145948117+GuardianChrisjay@users.noreply.github.com>
2023-10-05 12:53:25 +0800

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,11 @@
+///if 
+
+let firstFavNum = 22;
+let secondFavNum = 25;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNUm);
+console.log(firstFavNum / secondFavNum);
+let power = Math.pow(firstFavNum);
+console.log(power);
